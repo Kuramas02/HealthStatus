52a2833557a6de29f61bb12b36a2297533e93928
Kurama <91956903+Kuramas02@users.noreply.github.com>
2021-11-21 13:46:31 +0500

    Add files via upload

---
+++ HealthStatus/Config.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Exiled.API.Interfaces;
+
+namespace HealthStatus
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        
+
+        [Description("Normal hp status (100 - 90 hp)")]
+
+        public string Normal { get; set; } = "Normal HP Status";
+        public string NormalColor { get; set; } = "lime";
+        
+        [Description("light hp status (90 - 50 hp)")]
+
+        public string Light { get; set; } = "Light HP Status";
+        public string LightColor { get; set; } = "green";
+        
+        [Description("badly hp status (50 - 20 hp)")]
+
+        public string Badly { get; set; } = "Badly HP Status";
+        public string BadlyColor { get; set; } = "orange";
+        
+        [Description("super badly hp status (20 - 1 hp)")]
+
+        public string Dead { get; set; } = "Super Badly HP Status";
+        public string DeadColor { get; set; } = "red";
+
+
+    }
+}
---
+++ HealthStatus/EventHandlers.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using Exiled.Events.EventArgs;
+using MEC;
+
+namespace HealthStatus
+{
+    public class EventHandlers
+    {
+        public static void OnMain(HurtingEventArgs ev)
+        {
+
+            if (ev.Target.Health >= 90)
+            {
+                ev.Target.RankName = string.Empty;
+                Timing.CallDelayed(delay: 0, () =>
+                {
+                    ev.Target.RankName = ($"{Plugin.Instance.Config.Normal}");
+                    ev.Target.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                });
+            }
+            else if (ev.Target.Health >= 50)
+            {
+                ev.Target.RankName = string.Empty;
+                Timing.CallDelayed(delay: 0, () =>
+                {
+                    ev.Target.RankName = ($"{Plugin.Instance.Config.Light}");
+                    ev.Target.RankColor = $"{Plugin.Instance.Config.LightColor}";
+                });
+            }
+            else if (ev.Target.Health >= 20)
+            {
+                ev.Target.RankName = string.Empty;
+                Timing.CallDelayed(delay: 0, () =>
+                {
+                    ev.Target.RankName = ($"{Plugin.Instance.Config.Badly}");
+                    ev.Target.RankColor = $"{Plugin.Instance.Config.BadlyColor}";
+                });
+            }
+            else if (ev.Target.Health >= 1)
+            {
+                ev.Target.RankName = string.Empty;
+                Timing.CallDelayed(delay: 0, () =>
+                {
+                    ev.Target.RankName = ($"{Plugin.Instance.Config.Dead}");
+                    ev.Target.RankColor = $"{Plugin.Instance.Config.DeadColor}";
+                });
+            }
+        }
+
+        public static void OnUseHeal(UsedItemEventArgs ev)
+        {
+            if (ev.Item.IsUsing)
+            {
+                if (ev.Item.Type == ItemType.Adrenaline)
+                {
+                    if (ev.Player.Health >= 90)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 50)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 20)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Light}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.LightColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 1)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Badly}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.BadlyColor}";
+                        });
+                    }
+                }
+                if (ev.Item.Type == ItemType.Medkit)
+                {
+                    if (ev.Player.Health >= 90)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 50)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 20)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Light}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.LightColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 1)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Badly}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.BadlyColor}";
+                        });
+                    }
+                }
+                if (ev.Item.Type == ItemType.Painkillers)
+                {
+                    if (ev.Player.Health >= 90)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 50)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Light}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.LightColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 20)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Light}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.LightColor}";
+                        });
+                    }
+                    else if (ev.Player.Health >= 1)
+                    {
+                        ev.Player.RankName = string.Empty;
+                        Timing.CallDelayed(delay: 0, () =>
+                        {
+                            ev.Player.RankName = ($"{Plugin.Instance.Config.Badly}");
+                            ev.Player.RankColor = $"{Plugin.Instance.Config.BadlyColor}";
+                        });
+                    }
+                }
+            }
+        }
+
+        public static void OnDeath(DiedEventArgs ev)
+        {
+            Timing.CallDelayed(delay: 2, () =>
+            {
+                ev.Target.RankName = string.Empty;
+            });
+        }
+
+        public static void OnSpawn(SpawningEventArgs ev)
+        {
+            ev.Player.RankName = String.Empty;
+            Timing.CallDelayed(delay: 1, () =>
+            {
+                ev.Player.RankName = ($"{Plugin.Instance.Config.Normal}");
+                ev.Player.RankColor = $"{Plugin.Instance.Config.NormalColor}";
+            });
+        }
+
+    }
+}
